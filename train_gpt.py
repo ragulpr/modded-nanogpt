f94624fb857e12dabe571ebfd760bbdc0c766858
Egil Martinsson <egil.martinsson@gmail.com>
2025-01-20 22:42:01 -0800

    DEBUG oom

--- train_gpt.py
+++ train_gpt.py
@@ -467,7 +467,7 @@ class Hyperparameters:
     val_files = "data/fineweb10B/fineweb_val_*.bin" # input .bin to eval validation loss on
     val_tokens = 10485760 # how many tokens of validation data? it's important to keep this fixed for consistent comparisons
     # optimization
-    batch_size = 8*56*1024 # batch size in tokens
+    batch_size = 8*16*1024 # batch size in tokens
     num_iterations = 15 # number of iterations to run
     cooldown_frac = 0.4 # fraction of training spent cooling down the learning rate
     # evaluation and logging
