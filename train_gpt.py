d88236cbded6fcc683509c621a41e3e76a6b03e7
Egil Martinsson <egil.martinsson@gmail.com>
2025-03-31 14:25:29 -0700

    No effect.

--- train_gpt.py
+++ train_gpt.py
@@ -591,26 +591,6 @@ model: nn.Module = torch.compile(model, dynamic=False)
 warmup_steps = 10
 initial_state = dict(model=copy.deepcopy(model.state_dict()),
                      optimizers=[copy.deepcopy(opt.state_dict()) for opt in optimizers]) # save the initial state
-## DEBUG
-# PRE-COMPILE
-print0(f'Compile todo: {torch.cuda.memory_allocated() // 1024 // 1024}MB | cumem peak: {torch.cuda.max_memory_allocated() // 1024 // 1024} MiB', console=True)
-inputs = targets = torch.randint(0, args.vocab_size, size=(args.val_seq_len,), device="cuda")
-inputs = inputs.to(torch.int32)
-for name, module in model.named_modules():
-    if isinstance(module, TailDropout):
-        module.set_k(0)
-with torch.no_grad():
-    model(inputs, targets, get_window_size_blocks(0))
-model.eval() # Reset k
-for name, module in model.named_modules():
-    if isinstance(module, TailDropout):
-        module.set_k(0)
-        with torch.no_grad():
-            model(inputs, targets, get_window_size_blocks(0))
-        module.set_k(None)
-model.train() # Reset k
-print0(f'Compile done: {torch.cuda.memory_allocated() // 1024 // 1024}MB | cumem peak: {torch.cuda.max_memory_allocated() // 1024 // 1024} MiB', console=True)
-### DEBUG
 
 for _ in range(warmup_steps):
     inputs = targets = torch.randint(0, args.vocab_size, size=(args.train_seq_len,), device="cuda")
@@ -742,23 +722,26 @@ print0("WORKS before set_k with long seq_len..",console=True)
 k_iterator = [0, 1, 2, 4, 8, 16] + list(range(32, 768+1, 32))
 
 # PRE-COMPILE
+print0(f'Compile todo: {torch.cuda.memory_allocated() // 1024 // 1024}MB | cumem peak: {torch.cuda.max_memory_allocated() // 1024 // 1024} MiB', console=True)
 inputs = targets = torch.randint(0, args.vocab_size, size=(args.val_seq_len,), device="cuda")
 inputs = inputs.to(torch.int32)
 for name, module in model.named_modules():
     if isinstance(module, TailDropout):
         module.set_k(0)
 with torch.no_grad():
-    model(inputs, targets, get_window_size_blocks(0))
+    for _ in range(warmup_steps):
+        model(inputs, targets, get_window_size_blocks(0))
 model.eval() # Reset k
 for name, module in model.named_modules():
     if isinstance(module, TailDropout):
         module.set_k(0)
         with torch.no_grad():
-            model(inputs, targets, get_window_size_blocks(0))
+            for _ in range(warmup_steps):
+                model(inputs, targets, get_window_size_blocks(0))
         module.set_k(None)
-model.eval() # Reset k
+model.train() # Reset k
+print0(f'Compile done: {torch.cuda.memory_allocated() // 1024 // 1024}MB | cumem peak: {torch.cuda.max_memory_allocated() // 1024 // 1024} MiB', console=True)
 
-print0(f'Compiled @ all k. eval cumem: {torch.cuda.memory_allocated() // 1024 // 1024}MB | cumem peak: {torch.cuda.max_memory_allocated() // 1024 // 1024} MiB', console=True)
 
 kind = "k @ all layers"
 print0(f"{kind} ({training_time_ms + 1000 * (time.perf_counter() - t0):.0f})", console=True)
